68d146b65a96df65c18568d865cf6850584c6d99
Venky Iyer <indigoviolet@gmail.com>
2024-11-08 18:15:36 -0800

    rework with bugfix on (point), keep track of node

--- ts-movement-simple.el
+++ ts-movement-simple.el
@@ -13,57 +13,75 @@
 ;;; Code:
 (require 'treesit)
 
-(defun tsms/node-prev (point)
-  "Go to previous sibling of node at POINT and go to start of node."
-  (interactive "d")
-  (when-let* ((node (treesit-node-at point))
+(defvar-local tms--current-node nil
+  "Current tree-sitter node being navigated.")
+
+(defun tms--get-or-init-node ()
+  "Get current node or initialize it to node at point."
+  (or tms--current-node
+    (setq tms--current-node (treesit-node-at (point)))))
+
+(defun tms/node-prev ()
+  "Go to previous sibling of current node and go to start of node."
+  (interactive)
+  (when-let* ((node (tms--get-or-init-node))
                (prev (treesit-node-prev-sibling node)))
+    (setq tms--current-node prev)
     (goto-char (treesit-node-start prev))))
 
-(defun tsms/node-next (point)
-  "Go to next sibling of node at POINT and go to start of node."
-  (interactive "d")
-  (when-let* ((node (treesit-node-at point))
+(defun tms/node-next ()
+  "Go to next sibling of current node and go to start of node."
+  (interactive)
+  (when-let* ((node (tms--get-or-init-node))
                (next (treesit-node-next-sibling node)))
+    (setq tms--current-node next)
     (goto-char (treesit-node-start next))))
 
-(defun tsms/node-parent (point)
-  "Go to parent of node at POINT."
-  (interactive "d")
-  (when-let* ((node (treesit-node-at point))
+(defun tms/node-parent ()
+  "Go to parent of current node."
+  (interactive)
+  (when-let* ((node (tms--get-or-init-node))
                (parent (treesit-node-parent node)))
+    (setq tms--current-node parent)
     (goto-char (treesit-node-start parent))))
 
-(defun tsms/node-child (point)
-  "Go to first child containing POINT."
-  (interactive "d")
-  (when-let* ((node (treesit-node-at point))
-               (child (treesit-node-first-child-for-pos node point)))
+(defun tms/node-child ()
+  "Go to first child containing point."
+  (interactive)
+  (when-let* ((node (tms--get-or-init-node))
+               (child (treesit-node-first-child-for-pos node (point))))
+    (setq tms--current-node child)
     (goto-char (treesit-node-start child))))
 
-(defun tsms/node-start (point)
-  "Go to start of node at POINT."
-  (interactive "d")
-  (when-let ((node (treesit-node-at point)))
+(defun tms/node-start ()
+  "Go to start of current node."
+  (interactive)
+  (when-let ((node (tms--get-or-init-node)))
     (goto-char (treesit-node-start node))))
 
-(defun tsms/node-end (point)
-  "Go to end of node at POINT."
-  (interactive "d")
-  (when-let ((node (treesit-node-at point)))
+(defun tms/node-end ()
+  "Go to end of current node."
+  (interactive)
+  (when-let ((node (tms--get-or-init-node)))
     (goto-char (1- (treesit-node-end node)))))
+
+(defun tms/reset-node ()
+  "Reset the current node to the one at point."
+  (interactive)
+  (setq tms--current-node (treesit-node-at (point))))
 
 ;;;###autoload
 (defvar ts-movement-simple-map
-  (let ((map (make-sparse-keymap "Tree Sitter Movement")))
+  (let ((map (make-sparse-keymap "Tree Sitter Simple Movement")))
     ;; Add your preferred keybindings here
     map)
-  "Keymap for `ts-movement-mode'.")
+  "Keymap for `ts-movement-simple-mode'.")
 
 ;;;###autoload
 (define-minor-mode ts-movement-simple-mode
   "Movement commands using treesit syntax tree."
-  :keymap ts-movement-simple-map)
+  :keymap ts-movement-simple-map
+  (setq-local tms--current-node nil))
 
 (provide 'ts-movement-simple)
 ;;; ts-movement-simple.el ends here
